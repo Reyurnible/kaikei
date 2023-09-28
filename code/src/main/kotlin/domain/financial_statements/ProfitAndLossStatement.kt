d0c6522f1bb1b92c982d61f9e58d59213c342b24
reyurnible <reyurnible@gmail.com>
2023-09-28 09:24:33 +0900

    :+1: 財務諸表の空クラスを追加

---
+++ code/src/main/kotlin/domain/financial_statements/ProfitAndLossStatement.kt
@@ -0,0 +1,15 @@
+package domain.financial_statements
+
+/**
+ * 損益計算書
+ *
+ * 略してP/Lとも呼ばれる。米国では、Income Statement と呼ぶことも多い
+ * 役割は、「一定期間の経営成績」を示すこと
+ *
+ * 損益計算書は「収益」と「費用」、「利益」から構成
+ */
+// @TODO 損益計算書の項目を記載
+class ProfitAndLossStatement : FinancialStatements() {
+
+
+}
---
+++ code/src/main/kotlin/domain/financial_statements/ShareholdersEquityStatement.kt
@@ -0,0 +1,16 @@
+package domain.financial_statements
+
+/**
+ * 株主資本等変動計算書
+ *
+ * Statements of Shareholder’s Equity、略して S/S と 呼ばれる。
+ * 役割は、「貸借対照表の純資産の変動状況」を示すこと
+ *
+ * 2006年の新会社法が施行される前は、利益処分計算書という財務諸表があり、新会社法によって株主資本等変動計算書となった。
+ * 純資産の部の項目が表の列として表現され、一番上の行は純資産の部の「当期首残高」、一番下の行が「当期末残高」となっている。
+ */
+// @TODO 株主資本等変動計算書の項目を記載
+class ShareholdersEquityStatement : FinancialStatements() {
+
+
+}
