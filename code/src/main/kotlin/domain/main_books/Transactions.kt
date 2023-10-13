42d6c7d1cc130003f24133ae73c5b4fb51cf36c1
reyurnible <reyurnible@gmail.com>
2023-10-13 09:36:36 +0900

    :recycle: 仕訳取引の各行に借方・貸方を持つ形に修正

--- code/src/main/kotlin/domain/main_books/Transactions.kt
+++ code/src/main/kotlin/domain/main_books/Transactions.kt
@@ -8,13 +8,32 @@ import java.time.LocalDate
  * 未仕分けの取引を扱わないため、取引とだけ表現する。
  *
  * @param date 取引日。紙での記入時は、月と日を記載する
- * @param deposit 借(り)方勘定
- * @param credit 貸(し)方勘定
+ * @param accountItemList 仕訳された各一行。
  * @param explanation 取引内容（摘要に、勘定科目と取引内容を記載するがその中の取引内容のみ）
+ * @property debit 借(り)方勘定
+ * @property credit 貸(し)方勘定
  */
 data class Transactions(
     val date: LocalDate,
-    val deposit: AccountItem,
-    val credit: AccountItem,
+    val accountItemList: List<AccountItem>,
     val explanation: String?,
-)
+) {
+    val debit: List<AccountItem>
+        get() = accountItemList.filter { it.transactionSide == TransactionSide.Debit }
+    val credit: List<AccountItem>
+        get() = accountItemList.filter { it.transactionSide == TransactionSide.Credit }
+
+    init {
+        verify()
+    }
+
+    private fun verify() {
+        // 中身が0件はNG
+        check(accountItemList.isNotEmpty())
+        // 借方・貸方でどちらかしか存在しないのはNG
+        check(debit.isNotEmpty())
+        check(credit.isNotEmpty())
+        // 借方・貸方で金額が合わないのはNG
+        check(debit.sumOf { it.amount } == credit.sumOf { it.amount })
+    }
+}
